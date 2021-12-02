2dc6ecdc79366025b25ff95c9f1c8c14e2b92d38
welt <richard.weltman@havas.com>
2021-12-02 20:32:45 +0000

    adds sql dev snippets to repo

---
+++ mysql/create-note.sql
@@ -0,0 +1,7 @@
+INSERT INTO `Notes` (`id`, `date`, `title`, `slug`, `description`, `content`, `createdAt`, `updatedAt`)
+VALUES
+	(1, '2021-11-28', 'lorem ipsum', 'lorem-ipsum', 'test note', 'lorem ipsum dolor sit amet', '2010-11-28 00:00:00', '2021-11-30 00:00:00'),
+	(2, '2021-11-29', 'My Test', 'my-test', 'test note', 'lorem ipsum dolor sit amet', '2010-11-29 00:00:00', '2021-11-30 00:00:00'),
+	(3, '2021-12-01', 'Another test', 'another-test', 'making another note', 'Cras eget sodales magna. Sed commodo accumsan quam.', '2021-12-01 20:42:56', '2021-12-01 20:42:56'),
+	(4, '2021-12-01', 'EHLO', 'ehlo', 'oineoun', 'Curabitur pharetra feugiat nulla sit amet pretium. Phasellus ac maximus leo, non fermentum mi. ', '2021-12-01 20:45:03', '2021-12-01 20:45:03'),
+	(5, '2021-12-01', 'moinmoin', 'moinmoin', 'moin oinoun oiunoun', 'Fusce a imperdiet augue, ac aliquet risus. Nulla tincidunt metus eu augue vulputate auctor. Aenean rutrum ut neque vel semper. ', '2021-12-01 20:49:45', '2021-12-01 20:49:45');
